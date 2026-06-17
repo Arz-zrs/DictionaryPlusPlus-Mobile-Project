8dac542167bae03838e0186619743bcde4fe9465
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2026-06-17 15:36:40 +0800

    init: setting up project structure and color palletes

--- app/src/main/java/com/example/dictionaryplusplus/ui/theme/Color.kt
+++ app/src/main/java/com/example/dictionaryplusplus/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package com.example.dictionaryplusplus.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Dark Theme Colors
+val PrimaryDark = Color(0xFFC9A84C)
+val BackgroundDark = Color(0xFF111111)
+val SurfaceDark = Color(0xFF1A1A16)
+val OnBackgroundDark = Color(0xFFF5F0E0)
+val OnSurfaceDark = Color(0xFFF5F0E0)
+val SecondaryDark = Color(0xFF888780)
+val OutlineDark = Color(0xFF2A2A22)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Light Theme Colors
+val PrimaryLight = Color(0xFFB8860B)
+val BackgroundLight = Color(0xFFFAF8F3)
+val SurfaceLight = Color(0xFFFFFFFF)
+val OnBackgroundLight = Color(0xFF1A1A16)
+val OnSurfaceLight = Color(0xFF1A1A16)
+val SecondaryLight = Color(0xFF6B6A62)
+val OutlineLight = Color(0xFFE5E2D8)
+
+// Common Colors
+val Success = Color(0xFF1D9E75)
+val ErrorRed = Color(0xFFD32F2F)
