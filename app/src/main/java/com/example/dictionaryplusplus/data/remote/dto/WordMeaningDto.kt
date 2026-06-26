2172350d4e68298817687afb2235a2b3c51c7d03
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2026-06-26 16:56:37 +0800

    fix: clean up fetching

--- app/src/main/java/com/example/dictionaryplusplus/data/remote/dto/WordMeaningDto.kt
+++ app/src/main/java/com/example/dictionaryplusplus/data/remote/dto/WordMeaningDto.kt
@@ -3,7 +3,7 @@ package com.example.dictionaryplusplus.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class WordMeaningDto(
-    @SerializedName("partOfSpeech") val partOfSpeech: String?, // TODO: remove if unused
+    @SerializedName("partOfSpeech") val partOfSpeech: String?,
     @SerializedName("definitions") val definitions: List<WordDefinitionDto>?,
     @SerializedName("synonyms") val synonyms: List<String>?
 )
--- app/src/main/java/com/example/dictionaryplusplus/data/remote/dto/WordPhoneticDto.kt
+++ app/src/main/java/com/example/dictionaryplusplus/data/remote/dto/WordPhoneticDto.kt
@@ -3,6 +3,5 @@ package com.example.dictionaryplusplus.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class WordPhoneticDto(
-    @SerializedName("text") val text: String?,
-    @SerializedName("audio") val audio: String? // TODO: if I don't have time to implement audio playback, remove this
+    @SerializedName("text") val text: String?
 )
